30cc2d8d958f4fb371fb3e6f042ce5b656bc4fbd
Juan Cañar <jg.canarr@uea.edu.ec>
2024-08-30 00:57:08 -0500

    Deber de la semana 11 ordenamiento de Matriz

--- 3.2Operaciones con Arreglos/ejercicio2.py
+++ 3.2Operaciones con Arreglos/ejercicio2.py
@@ -1,7 +1,22 @@
 #ejercicio 2 Matriz de 3 x 3
 matriz = [
-    [82, 14, 67],
-    [20, 80, 90],
-    [56, 78, 99]
+    [19, 84, 47],
+    [56, 21, 76],
+    [69, 78, 89]
 ]
 
+print(matriz)
+#Metodo de Ordenamiento buble_sort
+def buble_sort(fila):
+    #algoritmo buble sort
+     n = len(fila)
+     for i in range(n):
+         for j in range(n-1, i, -1):
+             if fila[j] > fila[j-1]:
+                 fila[j], fila[j-1] = fila[j-1], fila[j]
+                 print(fila)
+
+print("matriz original")
+print(matriz)
+buble_sort(matriz[0])
+print(matriz)
